fee67b8482b7dcbdac8c0a5693ab45a6e87a25d3
BarisCihanoglu <bariscihanoglu4@gmail.com>
2024-02-15 13:58:27 +0300

    added the retrieval system

--- script.py
+++ script.py
@@ -1,12 +1,77 @@
-import pandas as pd
 import gensim
+from gensim.models import KeyedVectors
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
 
-# Preprocess the Dataset
+# 1.Preprocess the Dataset
 
-dataframe = pd.read_csv('./dataset.csv', usecols = [1])
+def createDataFrame(csvFileAddress):
+    return pd.read_csv(csvFileAddress, usecols = [1])
 
-content = dataframe.content.apply(gensim.utils.simple_preprocess)
+def preProcessDataFrame(dataFrame):
+    return dataFrame.content.apply(gensim.utils.simple_preprocess)
 
-# Utilize Word Embedding Model
+dataFrame = createDataFrame('./dataset.csv')
 
-word2vec = gensim.models.KeyedVectors.load_word2vec_format('../GoogleNews-vectors-negative300.bin', binary=True)
+preprocessedDataFrame = preProcessDataFrame(dataFrame)
+
+# 2.Utilize Word Embedding Model
+
+def getModel(preTrainedModelAddress):
+    word2vec = gensim.models.KeyedVectors.load_word2vec_format(
+        preTrainedModelAddress,
+        binary=True,
+        limit=1000 # to work faster
+    )
+    return word2vec
+
+def get_embeddings(tokens):
+    word_vectors = getModel('../GoogleNews-vectors-negative300.bin.gz')
+    embeddings = []
+    for token in tokens:
+        if token in word_vectors:
+            embeddings.append(word_vectors[token])
+    if embeddings:
+        return np.mean(embeddings, axis=0)  # Average the embeddings
+    else:
+        return np.zeros(word_vectors.vector_size)
+
+# 3.Retrieval System
+
+query = "office at 5am"
+
+# Preprocess and get the embedding for the query
+def get_query_embedding(query):
+    tokens = list(gensim.utils.tokenize(query, to_lower=True, deacc = True))
+    embedding = get_embeddings(query)
+    return embedding
+
+# Compute similarity between the query and each note
+def compute_similarity(query_embedding, note_embeddings):
+    similarities = cosine_similarity([query_embedding], note_embeddings)
+    return similarities[0]
+
+# Find the most relevant note
+def retrieve_most_relevant_note(query, dataFrame, note_embeddings):
+    query_embedding = get_query_embedding(query)
+    similarities = compute_similarity(query_embedding, note_embeddings)
+    most_similar_index = np.argmax(similarities)
+    return most_similar_index
+
+# Embed all notes in the dataframe
+note_embeddings = [get_embeddings(tokens) for tokens in preprocessedDataFrame]
+
+# print(dataFrame[1])
+
+print(retrieve_most_relevant_note(
+    query,
+    preprocessedDataFrame,
+    note_embeddings
+))
+
+# print(dataFrame[retrieve_most_relevant_note(
+#     query,
+#     preprocessedDataFrame,
+#     note_embeddings
+# )])
