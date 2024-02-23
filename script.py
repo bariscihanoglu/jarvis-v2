54bca427c8c94653543ef37d25dd6ab290e98c98
Barış Cihanoğlu <bariscihanoglu4@gmail.com>
2024-02-23 22:37:29 +0300

    added mechanism to retrieve more than 1 notes. Also added a threshold of similarity.

--- script.py
+++ script.py
@@ -4,16 +4,12 @@ import time
 import pandas as pd
 import numpy as np
 
+# Give necessary addressses for model and dataset.
 model_address = '../GoogleNews-vectors-negative300.bin.gz'
 dataset_address = './dataset.csv'
 
-# Load Word2Vec model
-
-word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(
-    model_address,
-    binary=True,
-    limit=20000 # Limit of words (GoogleNews vector contains words with their prevalence of use)
-)
+# How many output do you want to see? (Maximum)
+retrieve_count = 3
 
 # 1.Preprocess the Dataset
 
@@ -22,9 +18,6 @@ def createDataFrame(csv_file_address):
 
 def preProcessDataFrame(data_frame):
     return data_frame.content.apply(gensim.utils.simple_preprocess)
-
-data_frame = createDataFrame(dataset_address)
-preprocessed_data_frame = preProcessDataFrame(data_frame)
 
 # 2.Utilize Word Embedding Model
 
@@ -53,11 +46,28 @@ def compute_similarity(query_embedding, note_embeddings):
 # Find the most relevant note
 def retrieve_most_relevant_note_index(query, note_embeddings, model):
     query_embedding = get_query_embedding(query, model)
-    if query_embedding[0] == -1: # If can't find any match in model for any of the words
-        return -1
+    if query_embedding[0] == -1:
+        return []
+    
     similarities = compute_similarity(query_embedding, note_embeddings)
-    most_similar_index = np.argmax(similarities)
-    return most_similar_index
+    most_similar_indexes = np.argsort(similarities)[::-1][:retrieve_count]
+    most_similar_indexes = [i for i in most_similar_indexes if similarities[i] > 0.3]
+    
+    # use this one to retrieve only the most relevant.
+    # most_similar_indexes = np.argmax(similarities)
+    return most_similar_indexes
+
+
+# Load Word2Vec model
+word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(
+    model_address,
+    binary=True,
+    limit=20000 # Limit of words (GoogleNews vector contains words with their prevalence of use)
+)
+
+# Create data frame and preprocess
+data_frame = createDataFrame(dataset_address)
+preprocessed_data_frame = preProcessDataFrame(data_frame)
 
 # Embed all notes in the dataframe
 note_embeddings = [get_embeddings(tokens, word2vec_model) for tokens in preprocessed_data_frame]
@@ -67,12 +77,15 @@ while True:
     query = input("\nJarvis-Mark2: Hello! What do you want to learn about the company?\n\nUser: ")
 
     # Retrieve the most relevant note
-    most_relevant_note_index = retrieve_most_relevant_note_index(query, note_embeddings, word2vec_model)
+    most_relevant_note_indexes = retrieve_most_relevant_note_index(query, note_embeddings, word2vec_model)
 
-    if most_relevant_note_index == -1:
+    if not (most_relevant_note_indexes):
+        time.sleep(1)
         print("\nJarvis-Mark2: I couldn't find anything related in the database!")
+        time.sleep(3)
     else:
         print("\nJarvis-Mark2: Sure! I found this on my database:\n")
         time.sleep(1)
-        print(f"Data {most_relevant_note_index + 1}: {data_frame['content'][most_relevant_note_index]}")
+        for most_relevant_note_index in most_relevant_note_indexes:
+            print(f"Data {most_relevant_note_index + 1}: {data_frame['content'][most_relevant_note_index]}")
         time.sleep(3)
